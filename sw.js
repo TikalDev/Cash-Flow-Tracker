5155a4964888863ac49aae4f6d724ae2c2236b17
Jam8588 <joel@atmosinc.ca>
2026-07-08 17:17:42 -0400

    Update service worker to version 17 and improve caching

--- sw.js
+++ sw.js
@@ -1,30 +1,15 @@
-const CACHE = "cashflow-v16";
-const ASSETS = ["index.html", "manifest.json", "icon-192.png", "icon-512.png"];
-
-self.addEventListener("install", (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET") return;
+const CACHE = "cashflow-v17";
+const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{}))); });
+self.addEventListener("activate", e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener("fetch", e=>{
+  const url=new URL(e.request.url);
+  // Never cache Supabase or other cross-origin API calls; always go to network
+  if(url.origin!==location.origin){ return; }
   e.respondWith(
-    caches.match(e.request).then((cached) => {
-      const network = fetch(e.request)
-        .then((res) => {
-          const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
-          return res;
-        })
-        .catch(() => cached);
-      return cached || network;
-    })
+    fetch(e.request).then(resp=>{
+      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy).catch(()=>{}));
+      return resp;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html")))
   );
 });
