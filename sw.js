8cd1443c75aa17811fd04455f2b51333576efe30
Jam8588 <joel@atmosinc.ca>
2026-07-08 13:10:08 -0400

    Update cache version from v1 to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cashflow-v1";
+const CACHE = "cashflow-v2";
 const ASSETS = ["index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", (e) => {
