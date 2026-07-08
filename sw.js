e2acda1bc88c88d27220943291f2a13d50887b2c
Jam8588 <joel@atmosinc.ca>
2026-07-08 15:15:41 -0400

    Update cache version from v8 to v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cashflow-v8";
+const CACHE = "cashflow-v10";
 const ASSETS = ["index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", (e) => {
